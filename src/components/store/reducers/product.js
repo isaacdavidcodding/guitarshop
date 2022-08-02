7e693c559f3ed64ee3b4353ab6cad267425f0df3
Isaac David <isaacdavid797@gmail.com>
2022-08-02 18:13:08 -0300

    Fork de projeto desenvolvido em bootcamp da DIO

---
+++ src/components/store/reducers/product.js
@@ -0,0 +1,136 @@
+const productsList = [
+  {
+    id_product: 1,
+    category: "Fender",
+    fk_idcategorys: 1,
+    name_product: "Guitarra elétrica SX Vintage Series SST57",
+    price: 300,
+    image: "./images/produtos/fender_sx_vintage.webp",
+    id_categorys: 1,
+    name_categorys: "Fender",
+    selected: false
+  },
+  {
+    id_product: 2,
+    category: "Fender",
+    fk_idcategorys: 1,
+    name_product: "Guitarra elétrica Squier by Fender Contemporary",
+    price: 350,
+    image: "./images/produtos/fender_squier_telehh.webp",
+    id_categorys: 1,
+    name_categorys: "Fender",
+    selected: false
+  },
+  {
+    id_product: 9,
+    category: "Gibson",
+    fk_idcategorys: 2,
+    name_product: "Guitarra elétrica Epiphone Les Paul Standard 50s",
+    price: 230,
+    image: "./images/produtos/gibson_epiphonestd50.webp",
+    id_categorys: 2,
+    name_categorys: "Gibson",
+    selected: false
+  },
+  {
+    id_product: 4,
+    category: "Ibanez",
+    fk_idcategorys: 3,
+    name_product: "Guitarra elétrica Ibanez RG GIO GRG131DX",
+    price: 399.99,
+    image: "./images/produtos/ibanez_vermelho.webp",
+    id_categorys: 3,
+    name_categorys: "Ibanez",
+    selected: false
+  },
+  {
+    id_product: 10,
+    category: "Ibanez",
+    fk_idcategorys: 3,
+    name_product: "Guitarra elétrica Ibanez PGM/FRM PGMM31",
+    price: 350.99,
+    image: "./images/produtos/ibanez_pgm_frm.webp",
+    id_categorys: 3,
+    name_categorys: "Ibanez",
+    selected: false
+  },
+  {
+    id_product: 11,
+    category: "Ibanez",
+    fk_idcategorys: 3,
+    name_product: "Guitarra elétrica Ibanez RG GIO GRG170DX",
+    price: 299.99,
+    image: "./images/produtos/ibanez_rggio_grg170dx.webp",
+    id_categorys: 3,
+    name_categorys: "Ibanez",
+    selected: false
+  },
+  {
+    id_product: 6,
+    category: "Gibson",
+    fk_idcategorys: 2,
+    name_product: "Guitarra elétrica Gibson SG para canhoto",
+    price: 200,
+    image: "./images/produtos/gibson_std_sg.webp",
+    id_categorys: 2,
+    name_categorys: "Gibson",
+    selected: false
+  },
+  {
+    id_product: 7,
+    category: "Jackson",
+    fk_idcategorys: 4,
+    name_product: "Guitarra elétrica Jackson JS Series JS12",
+    price: 450,
+    image: "./images/produtos/jackson_js_series_js12.webp",
+    id_categorys: 4,
+    name_categorys: "Jackson",
+    selected: false
+  },
+  {
+    id_product: 3,
+    category: "Jackson",
+    fk_idcategorys: 4,
+    name_product: "Guitarra elétrica Jackson JS Series JS11",
+    price: 350,
+    image: "./images/produtos/jackson_js_series_js11.webp",
+    id_categorys: 4,
+    name_categorys: "Jackson",
+    selected: false
+  },
+  {
+    id_product: 5,
+    category: "Ibanez",
+    fk_idcategorys: 3,
+    name_product: "Guitarra elétrica Ibanez 7 cordas RG GIO GRG7221",
+    price: 399,
+    image: "./images/produtos/ibanez_rg_gio_grg7221.webp",
+    id_categorys: 3,
+    name_categorys: "Ibanez",
+    selected: false
+  },
+  {
+    id_product: 8,
+    category: "Gibson",
+    fk_idcategorys: 2,
+    name_product: "Guitarra elétrica Les Paul Standard 50s",
+    price: 180,
+    image: "./images/produtos/gibson_standard50.webp",
+    id_categorys: 2,
+    name_categorys: "Gibson",
+    selected: false
+  },
+  {
+    id_product: 12,
+    category: "Fender",
+    fk_idcategorys: 1,
+    name_product: "Guitarra elétrica Fender Standard Telecaster ",
+    price: 189.99,
+    image: "./images/produtos/fender_telecaster_std.webp",
+    id_categorys: 1,
+    name_categorys: "Fender",
+    selected: false
+  }
+];
+
+export default productsList;
